91d489475757b6e4d35666e7bace67d725e4a747
leventehegedus <levi.hegedus92@gmail.com>
2023-07-15 10:37:03 +0200

    init and shuffle

--- labtech-memory/src/App.tsx
+++ labtech-memory/src/App.tsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const DEFAULT_SIZE = 16;
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [size, setSize] = useState(DEFAULT_SIZE)
+  const [cards, setCards] = useState<number[]>([])
+
+  useEffect(() => {
+    generateMemory(size)
+  }, [])
+
+  const generateNumbers = (size: number): number[] => {
+    const numbersArray: number[] = [];
+    for (let i = 0; i < size; i++) {
+      numbersArray.push(i);
+    }
+    return numbersArray;
+  }
+
+  const generateMemory = (size: number): void => {
+
+    const numbers = generateNumbers(size);
+    const random = shuffleArray(generateNumbers(size));
+    if (JSON.stringify(random) === JSON.stringify(numbers)) {
+      generateMemory(size);
+    }
+    setCards(random);
+  };
+
+  const shuffleArray = (array: number[]) => {
+    const length = array.length;
+    for (let i = length; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      const currentIndex = i - 1;
+      const temp = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
+    return array;
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {cards.map(card => (
+          <span>{card} </span>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        {cards.map(card => (
+          <span>{Math.floor(card / 2)} </span>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => generateMemory(size)}>new numbers</button>
     </>
   )
 }
